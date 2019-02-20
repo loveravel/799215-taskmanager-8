d61bb8fdd87696a7c35f68a29823b30fc2caf8cf
Odintsov Sergey <loveravel@yandex.ru>
2019-02-20 18:29:34 +0400

    Изменил способ получения случайного целого числа с rand на floor

--- public/main.js
+++ public/main.js
@@ -1,10 +1,10 @@
 'use strict';
 
 /* Функция для генерирования случайного числа от min до max */
-const getRandomInteger = (min, max) => {
-  let rand = min - 0.5 + Math.random() * (max - min + 1);
-  rand = Math.round(rand);
-  return rand;
+const getRandomInteger = (minimumNumber, maximumNumber) => {
+  let randomNumber = minimumNumber + Math.random() * (maximumNumber - minimumNumber + 1);
+  randomNumber = Math.floor(randomNumber);
+  return randomNumber;
 };
 
 /* Отрисовка фильтров */
